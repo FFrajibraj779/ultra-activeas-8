63a66ae6d00bcf307790c690f8ece12f81f387c5
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-09-29 18:49:46 +0600

    break time problem fixed

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,16 +1,28 @@
-import React from 'react';
+
 import './Profile.css'
 import image from '../image/my-bg.png'
-const Profile = ({timeBtn}) => {
-    // console.log(timeBtn);
-    let prevTime= 0;
-    for(const times of  timeBtn){
-       
-         prevTime= prevTime+ times.time
-    //  time=time+ time.time
-    
+import { useState } from 'react';
+
+
+
+const Profile = ({ timeBtn }) => {
+    const[dayBtn, setdayBtn]=useState(0)
    
+    // console.log(timeBtn);
+    let prevTime = 0;
+    for (const times of timeBtn) {
+        prevTime = prevTime + times.time
+        //  time=time+ time.time
     }
+ const handleBreakTime=(day)=>{
+    const newday= day;
+
+     setdayBtn(newday)
+
+ 
+
+ }
+
     return (
         <div className='profile-section'>
 
@@ -19,10 +31,8 @@ const Profile = ({timeBtn}) => {
                 <div className='name'>
                     <h3>Md Rajikul Islam</h3>
                     <p><i class="fa-solid fa-graduation-cap"></i>Student</p>
-
-                </div>
-
-            </div>
+                 </div>
+             </div>
 
             <div className="profile-desc">
                 <div>
@@ -37,17 +47,15 @@ const Profile = ({timeBtn}) => {
                     <p style={{ fontWeight: 'bold', fontSize: '25px' }}>18 <small className='weight'>yrs</small></p>
                     <p className='weight'>Age</p>
                 </div>
-
-
-            </div>
+        </div>
 
             <div >
                 <h3 className='break-title'>Add A Break</h3>
                 <div className='break-time'>
-                    <button className='day-btn'>8 day </button>
-                    <button className='day-btn'>18 day </button>
-                    <button className='day-btn'>21 day </button>
-                    <button className='day-btn'>28 day </button>
+                    <button onClick={()=>handleBreakTime(8)}  className='day-btn'> 8 <span>day</span>  </button>
+                    <button onClick={()=>handleBreakTime(18)}   className='day-btn'>18  <span>day</span> </button>
+                    <button onClick={()=>handleBreakTime(21)}  className='day-btn'>21  <span>day</span> </button>
+                    <button onClick={()=>handleBreakTime(28)}  className='day-btn'>28  <span>day</span> </button>
 
                 </div>
 
@@ -57,8 +65,8 @@ const Profile = ({timeBtn}) => {
                 <h3 className='break-title'>Working Details</h3>
 
                 <div className='working-details'>
-                    <p style={{fontWeight:'600'}}>Working time</p>
-                    <p style={{color:'#444', fontWeight:'800'}}> <span>{prevTime}</span> days</p>
+                    <p style={{ fontWeight: '600' }}>Working time</p>
+                    <p style={{ color: '#444', fontWeight: '800' }}> <span>{prevTime}</span> days</p>
                 </div>
             </div>
 
@@ -67,8 +75,8 @@ const Profile = ({timeBtn}) => {
                 <h3 className='break-title'>Break Time </h3>
 
                 <div className='working-details'>
-                    <p style={{fontWeight:'600'}}>Break time</p>
-                    <p style={{color:'#444', fontWeight:'800'}}>19 days</p>
+                    <p style={{ fontWeight: '600' }}>Break time</p>
+                    <p style={{ color: '#444', fontWeight: '800' }}> <span>{dayBtn}</span> days</p>
                 </div>
                 <div className='btn-done'>
 
