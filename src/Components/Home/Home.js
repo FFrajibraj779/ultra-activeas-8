1d94466c32d626d128e16e3c5cfd6bb7eaf81bb5
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-09-29 11:18:47 +0600

    responsive design added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,39 +2,47 @@ import React, { useEffect, useState } from 'react';
 import Profile from '../Profile/Profile';
 import SingleCart from '../SingleCart/SingleCart';
 import './Home.css'
+import img from '../image/work.jfif'
 
 const Home = () => {
-    const[carts, setCarts]=useState([]);
-    useEffect(()=>{
+    const [carts, setCarts] = useState([]);
+    useEffect(() => {
         fetch(`fakedata.json`)
-        .then(res=>res.json())
-        .then(data =>setCarts(data))
-      
-    },[])
+            .then(res => res.json())
+            .then(data => setCarts(data))
+
+    }, [])
     return (
         <div>
-             <div className="hom-img">
-                    
-                    <h1 className='text-5xl font-bold'>Work With Home</h1>
-                </div>
-                <div className='home-container '>
-
-            <div className="cart-container">
-               
-            
-            {
-            carts.map(cart =><SingleCart cart={cart} key={cart.id}></SingleCart>)
-            }
-            </div>
-
-            <div className="descriptio-container">
-
-                 <Profile></Profile>
-            </div>
            
+            <div className='home-container '>
+
+                <div>
+                <div className="home-info">
+                <img src={img} alt="" />
+
+                <h1>Work With Home</h1>
+            </div>
+            <div className='career-title'>
+                <h2>Selected your Career</h2>
+            </div>
+
+                     <div className='cart-container'>
+                        
+                    {
+                        carts.map(cart => <SingleCart cart={cart} key={cart.id}></SingleCart>)
+                    }
+                     </div>
+                </div>
+
+                <div className="descriptio-container">
+
+                    <Profile></Profile>
+                </div>
+
+            </div>
         </div>
-        </div>
-        
+
     );
 };
 
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -8,8 +8,8 @@ const Profile = () => {
             <div className="profile-img">
                 <img src={image} alt="" />
                 <div>
-                 <p>Md Rajikul Islam</p>
-                 <p>Student</p>
+                    <p>Md Rajikul Islam</p>
+                    <p>Student</p>
 
                 </div>
 
@@ -17,31 +17,31 @@ const Profile = () => {
 
             <div className="profile-desc">
                 <div>
-                    <p style={{fontWeight:'bold', fontSize:'30px'}}>60 <small style={{color:'light', fontWeight:'400',fontSize:'18px'}}>kg</small></p>
+                    <p style={{ fontWeight: 'bold', fontSize: '30px' }}>60 <small style={{ color: 'light', fontWeight: '400', fontSize: '18px' }}>kg</small></p>
                     <p>Weight</p>
                 </div>
                 <div>
-                    <p style={{fontWeight:'bold', fontSize:'30px'}}>6.4 <small style={{color:'light', fontWeight:'400',fontSize:'18px'}}>fet</small></p>
+                    <p style={{ fontWeight: 'bold', fontSize: '30px' }}>6.4 <small style={{ color: 'light', fontWeight: '400', fontSize: '18px' }}>fet</small></p>
                     <p>Height </p>
                 </div>
                 <div>
-                    <p style={{fontWeight:'bold', fontSize:'30px'}}>18 <small style={{color:'light', fontWeight:'400',fontSize:'18px'}}>yrs</small></p>
+                    <p style={{ fontWeight: 'bold', fontSize: '30px' }}>18 <small style={{ color: 'light', fontWeight: '400', fontSize: '18px' }}>yrs</small></p>
                     <p>Age</p>
                 </div>
-              
-                
+
+
             </div>
 
             <div>
                 <h2>Add A Break</h2>
                 <div className='break-time'>
-                 <p>8 day </p>
-                 <p>18 day </p>
-                 <p>21 day </p>
-                 <p>28 day </p>
+                    <p>8 day </p>
+                    <p>18 day </p>
+                    <p>21 day </p>
+                    <p>28 day </p>
 
                 </div>
-                
+
             </div>
 
             <div>
@@ -52,7 +52,7 @@ const Profile = () => {
                     <p>19 days</p>
                 </div>
             </div>
-          
+
 
             <div>
                 <h2>Break Time </h2>
@@ -63,7 +63,7 @@ const Profile = () => {
                 </div>
                 <div className='btn-done'>
 
-            <button className='complete-btn'>completed</button>
+                    <button className='complete-btn'>completed</button>
                 </div>
             </div>
         </div>
